f81e85721aab039277ce8db8f5555c85f04f8513
이은상 <kyrsang0801@inbrein.com>
2024-06-18 10:30:38 +0900

    Granted execution permission

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 
 mkdir $HOME/secrets
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASS_PHRASE" \
 --output $HOME/secrets/my_secret.json my_secret.json.gpg
